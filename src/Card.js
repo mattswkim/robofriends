637afd4ff429d7b50b54b3fed7670e1c4b7f1e54
Matt Kim <mattsunwoongkim@gmail.com>
2019-04-04 11:11:05 +0800

    initial Commit

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Card = ({
+    name,
+    email,
+    id
+  }) => {
+  return (
+    <div className="bg-light-green dib br3 ma2 grow bw2 shadow-s">
+      <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
