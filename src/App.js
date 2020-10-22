fce2ea54d6112b3129133edbeb80025353272e0d
Matt Kim <mattsunwoongkim@gmail.com>
2020-10-22 12:36:58 +0900

    minor change

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import './App.css';
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState('');
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response=> response.json())
     .then(users=> {setRobots(users)})
-    console.log(count)
-  },[count])
+  },[]) // only run if [] changes
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value)
@@ -27,7 +25,6 @@ function App() {
   return (
   <div className="tc">
     <h1 className="f1">RoboFriends</h1>
-    <button onClick={()=>setCount(count+1)}>Click me!</button>
     <SearchBox searchChange={onSearchChange}/>
     <CardList robots={filteredRobots}/>
   </div>
